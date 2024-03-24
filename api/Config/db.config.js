0201c9bbe3593ebdd1cf86c472a162b67baa7791
Aqib Jawad <aqibjawad123@gmail.com>
2024-03-25 00:30:22 +0500

    api push

--- api/Config/db.config.js
+++ api/Config/db.config.js
@@ -6,32 +6,17 @@ const mysql = require("mysql");
 const dbConn = mysql.createConnection({
 
   // You can change your host whre your host ip id is added
-  host: "u884338749_cryptotrade",
+  host: "127.0.0.1:3306",
 
   // User must not change (Donot change the user)
-  user: "u884338749_root", 
+  user: "u884338749_crypto_trade", 
 
   // if you want to add your password you can add thorugh server
-  password: "",
+  password: "=EJ!Bi1fG",
 
   // database name does not required to be change this is also ok for your website
-  database: "u884338749_cryptotrade",
+  database: "u884338749_crypto",
 });
-
-// const dbConn = mysql.createConnection({
-
-//  // You can change your host whre your host ip id is added
-//  host: "localhost",
-
-//  // User must not change (Donot change the user)
-//  user: "root", 
-
-//  // if you want to add your password you can add thorugh server
-//  password: "",
-
-//  // database name does not required to be change this is also ok for your website
-//  database: "cryptotrade",
-// });
 
 dbConn.connect(function (err) {
   if (err) throw err;
