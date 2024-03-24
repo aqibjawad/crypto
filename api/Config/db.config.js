8486ba1c2cbbaea6f4590f0e85b1ca8ba33ccd0d
Aqib Jawad <aqibjawad123@gmail.com>
2024-03-24 22:09:35 +0500

    api push

--- api/Config/db.config.js
+++ api/Config/db.config.js
@@ -1,35 +1,37 @@
 "use strict";
 const mysql = require("mysql");
-//local mysql db connection
-// const dbConn = mysql.createConnection({
 
-//   // You can change your host whre your host ip id is added
-//   host: "u884338749_cryptotrade",
-
-//   // User must not change (Donot change the user)
-//   user: "root", 
-
-//   // if you want to add your password you can add thorugh server
-//   password: "",
-
-//   // database name does not required to be change this is also ok for your website
-//   database: "u884338749_cryptotrade",
-// });
+// local mysql db connection
 
 const dbConn = mysql.createConnection({
 
- // You can change your host whre your host ip id is added
- host: "localhost",
+  // You can change your host whre your host ip id is added
+  host: "u884338749_cryptotrade",
 
- // User must not change (Donot change the user)
- user: "root", 
+  // User must not change (Donot change the user)
+  user: "u884338749_root", 
 
- // if you want to add your password you can add thorugh server
- password: "",
+  // if you want to add your password you can add thorugh server
+  password: "",
 
- // database name does not required to be change this is also ok for your website
- database: "cryptotrade",
+  // database name does not required to be change this is also ok for your website
+  database: "u884338749_cryptotrade",
 });
+
+// const dbConn = mysql.createConnection({
+
+//  // You can change your host whre your host ip id is added
+//  host: "localhost",
+
+//  // User must not change (Donot change the user)
+//  user: "root", 
+
+//  // if you want to add your password you can add thorugh server
+//  password: "",
+
+//  // database name does not required to be change this is also ok for your website
+//  database: "cryptotrade",
+// });
 
 dbConn.connect(function (err) {
   if (err) throw err;
